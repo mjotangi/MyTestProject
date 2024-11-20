0bb427e8467203d7d23f1818f0e1d5292dd5bfd3
mjotangi <mjotangi@asu.edu>
2024-11-20 12:32:37 -0800

    Initial commit

---
+++ Server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files like HTML, CSS, JS
+app.use(express.static('public'));
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello, I am server!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
