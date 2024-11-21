90e298c46883f29e32a5002ba740521c42e6a3ce
mjotangi <mjotangi@asu.edu>
2024-11-21 12:36:48 -0800

    Add new products route

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 
 // Define a simple route
 app.get('/', (req, res) => {
-  res.send('Hello, I am server!');
+  res.send('Hello, My server!');
 });
 
 // Start the server
